5e2c669cc9b4138487ed9782918bce2ae797895b
NataliTs <natikz20@ukr.net>
2023-03-01 22:46:24 +0200

    completed tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const itemDiv = galleryItems
+  .map(
+    item => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}"alt="${item.description}" />
+</a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', itemDiv);
+
+let galleryLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+  enableKeyboard: true,
+});
+
+console.log(gallery);
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
